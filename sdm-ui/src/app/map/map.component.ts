14c0b7c17f210bfb91173d1f270f19c48ff36f77
Ian Dykes <ian.dykes@gmail.com>
2020-04-08 11:33:33 +0100

    Added leaflet map

--- sdm-ui/src/app/map/map.component.ts
+++ sdm-ui/src/app/map/map.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  private map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 53.050039, -1.406985 ],
+      zoom: 15
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+  }
 }
