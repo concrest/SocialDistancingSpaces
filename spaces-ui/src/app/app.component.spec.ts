094dadea3ac8c4bd6e7405f06fc2f935dfd0b043
Ian Dykes <ian.dykes@gmail.com>
2020-04-10 11:24:39 +0100

    Added modules to remove warnings

--- spaces-ui/src/app/app.component.spec.ts
+++ spaces-ui/src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        AppRoutingModule,
+        MaterialModule,
+        NoopAnimationsModule
+      ]
     }).compileComponents();
   }));
 
